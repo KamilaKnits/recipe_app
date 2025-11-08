276559027554b61320fb6427e5fc5a0fa62d3fee
Jonathon Hinchley <jonathon@trackhawkgps.com>
2025-11-07 19:22:54 -0600

    fix allowed hosts

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-6d4^fhecepd0$=zs^ittoctk+p30mr#lhpq3ayd51s*b*p9q1d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'kamilka-recipes-jh-0d2bc9cde5a8.herokuapp.com'
+]
 
 
 # Application definition
