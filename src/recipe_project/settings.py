e5befb8e20c4eee7a6365faeddecbab7bc4c4b04
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-11-09 13:40:56 -0500

    add-recipe template

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
 }
 
 STORAGES = {
+    'default': {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    
     # Enable WhiteNoise's GZip (and Brotli, if installed) compression of static assets:
     # https://whitenoise.readthedocs.io/en/latest/django.html#add-compression-and-caching-support
     "staticfiles": {
